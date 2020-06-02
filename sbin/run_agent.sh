f4f3b11664d4310dffa7f49c6e8865aca4f41a27
Newnius <newnius.cn@gmail.com>
2020-06-02 22:00:52 +0800

    add glusterfs

--- sbin/run_agent.sh
+++ sbin/run_agent.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ip=`hostname --ip-address`
+
 docker run \
 	--gpus all \
 	--name yao-agent \
@@ -9,9 +11,14 @@ docker run \
 	-d \
 	--restart always \
 	--detach=true \
+	--publish 8000:8000 \
 	--env ClientID=$(hostname) \
 	--env ClientHost=$(hostname) \
+	--env ClientExtHost=${ip} \
+	--env Port=8000 \
+	--env HeartbeatInterval=5 \
 	--env KafkaBrokers=kafka-node1:9092,kafka-node2:9092,kafka-node3:9092 \
+	--env EnableEventTrigger=true \
 	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
 	quickdeploy/yao-agent
---
+++ sbin/start_glusterfs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Setup
+docker run \
+	--name gluster_server \
+	--restart always \
+	-d \
+	--privileged=true \
+	--net=host \
+	-v /data/gluster/configuration:/etc/glusterfs:z \
+	-v /data/gluster/metadata:/var/lib/glusterd:z \
+	-v /data/gluster/logs:/var/log/glusterfs:z \
+	-v /data/gluster/data:/data \
+	gluster/gluster-centos
+
+exit 0;
+
+
+# Init
+gluster peer probe 192.168.100.101
+gluster peer probe 192.168.100.102
+gluster peer probe 192.168.100.103
+gluster peer probe 192.168.100.104
+gluster peer probe 192.168.100.105
+gluster peer probe 192.168.100.106
+
+
+# Create Volume
+gluster volume create yao replica 3 192.168.100.101:/data/yao 192.168.100.102:/data/yao 192.168.100.103:/data/yao 192.168.100.104:/data/yao 192.168.100.105:/data/yao 192.168.100.106:/data/yao
+
+
+# Client Mount
+sudo yum install glusterfs glusterfs-fuse attr -y
+
+sudo mkdir -p /dfs && sudo chmod -R 777 /dfs
+
+sudo mount -t glusterfs 192.168.100.102:/yao /dfs
+
+sudo chmod 777 /dfs
